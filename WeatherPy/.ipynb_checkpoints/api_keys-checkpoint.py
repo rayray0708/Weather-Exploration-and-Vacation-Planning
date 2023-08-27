093a8fc95bda5e8fd60221f6ace331a9934dc971
rayray0708 <rachelmmm@192-168-1-110.tpgi.com.au>
2023-08-27 12:01:32 +0800

    upload

--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "82592edb295a059a1a6f8917456e739f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "0385fdfeb3f04aa997c0d53e752c12ae"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "82592edb295a059a1a6f8917456e739f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "0385fdfeb3f04aa997c0d53e752c12ae"
